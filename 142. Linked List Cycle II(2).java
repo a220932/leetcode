35fe83969d38a0a8691e6b5603fceff57156e766
a220932 <88019240+a220932@users.noreply.github.com>
2022-10-17 20:26:34 +0800

    Create 142. Linked List Cycle II(2).java

---
+++ 142. Linked List Cycle II(2).java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        do {
+            if (fast == null || fast.next == null) return null;
+            slow = slow.next;
+            fast = fast.next.next;
+        } while (slow != fast);
+        
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
+        
+    }
+}
