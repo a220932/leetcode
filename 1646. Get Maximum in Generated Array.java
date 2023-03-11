0ad77dfff5019863684b1b37759502ba1ea97d00
a220932 <88019240+a220932@users.noreply.github.com>
2023-03-11 22:14:38 +0800

    Create 1646. Get Maximum in Generated Array.java

---
+++ 1646. Get Maximum in Generated Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        if (n == 0)return 0;
+        int[] nums = new int[n+1];
+        nums[0] = 0;
+        nums[1] = 1;
+        int max = 1;
+        for (int i = 2; i <= n; i++){
+            nums[i] = nums[i/2];
+            if (i % 2 == 1)
+                nums[i] += nums[(i/2) + 1];
+
+            if(max < nums[i])
+                max = nums[i];
+        }
+        return max;
+    }
+}
